0637edea49d9473e89398dde18dc7e19b4ec3420
hasan469328 <hasanur.68.bd@gmail.com>
2023-03-29 11:09:38 +0600

    navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import logo from '../../images/Logo.svg'
 const Header = () => {
   return (
-    <div className='bg-[#1C2B35] text-white p-4'>
-      <h1>This is my Header</h1>
-      <div>
-        
+    <div className='bg-[#1C2B35] h-[80px] text-white flex justify-between items-center lg:px-24'>
+      <img src={logo} alt="" className='max-w-fit'/>
+      <div className='flex gap-x-1 md:gap-x-2 lg:gap-x-4'>
+        <li className='list-none'><a href="">Order</a></li>
+        <li className='list-none'><a href="">Order-Review</a></li>
+        <li className='list-none'><a href="">Manage Inventory</a></li>
+        <li className='list-none'><a href="">Login</a></li>
       </div>
     </div>
   );
